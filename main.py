0f05913b1331d19c06dd00ee0ac774bfaa1097d4
wsz <1175690167@qq.com>
2023-05-08 17:26:22 +0800

    feat: 配置跨域

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import random
 import json
 import uvicorn
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 from tortoise.contrib.fastapi import register_tortoise
 from utils.globalq import tasks_queue
 from routers.job import router as job_router
-from routers.test import router as test_router
 from routers.comment import router as comment_router
 from utils.logger import log
 from spiders.xhs.spider import craw_xhs
@@ -21,8 +21,26 @@ with open("./configs/config.txt", "r") as f:
 
 app = FastAPI()
 
+# 处理跨域
+# 配置允许域名
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+# 处理跨域
+# 配置允许域名列表、允许方法、请求头、cookie等
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(job_router, prefix="/jobs")
-app.include_router(test_router, prefix="/test")
 app.include_router(comment_router, prefix="/comments")
 
 # 注册数据库连接
