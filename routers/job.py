e011c9470987e17502c57ae90f4181a12576fb4a
wsz <1175690167@qq.com>
2023-05-08 17:27:30 +0800

    perf: 添加接口文档注释; 优化代码

--- routers/job.py
+++ routers/job.py
@@ -3,12 +3,12 @@
 # @Author : 王思哲
 # @File : job.py
 # @Software: PyCharm
-from fastapi import APIRouter
-from uuid import uuid4
-from pydantic import BaseModel
 from typing import Dict
-from utils.globalq import tasks_queue
+from uuid import uuid4
+from fastapi import APIRouter, Body
+from pydantic import BaseModel
 from models.job import Job
+from utils.globalq import tasks_queue
 from utils.logger import debug
 
 
@@ -22,9 +22,24 @@ class JobCreateRequest(BaseModel):
 router = APIRouter()
 
 
-# 创建任务
-@router.post("/", description="创建爬虫任务")
-async def create_job(job_request: JobCreateRequest):
+# 创建爬虫任务
+@router.post("", description="创建爬虫任务")
+async def create_job(job_request: JobCreateRequest = Body(example={
+    "search_param": "搜索关键词",
+    "search_platform": "xhs|bilibili|douyin|weibo",
+    "search_size": {
+        "douyin": 10,
+        "weibo": 10,
+        "xhs": 1,
+        "bilibili": 1
+    },
+    "cookie": {
+        "douyin": "用户的cookie",
+        "weibo": "用户的cookie",
+        "xhs": "用户的cookie",
+        "bilibili": "用户的cookie"
+    }
+})):
     job_id = str(uuid4())
     job = Job(
         job_id=job_id,
@@ -45,19 +60,11 @@ async def create_job(job_request: JobCreateRequest):
 
     debug(f"/jobs/  请求参数:   {job_request}")
     await job.save()
-    return {"job_id": job.job_id, "search_param": job.search_param,
-            "search_platform": job.search_platform,
-            "create_time": job.create_time, "status": job.status}
+    return job.__dict__
 
 
 # 获取任务列表
-@router.get("/", description="获取爬虫任务列表")
+@router.get("", description="获取爬虫任务列表")
 async def get_jobs():
     jobs = await Job.all()
-    return [{
-        "job_id": job.job_id,
-        "search_param": job.search_param,
-        "search_platform": job.search_platform,
-        "create_time": job.create_time,
-        "status": job.status
-    } for job in jobs]
+    return [job.__dict__ for job in jobs]
